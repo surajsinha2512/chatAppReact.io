cee94ffbb8af88a5ed370e403ffebc4c6e73efb9
surajsinha2512 <61259615+surajsinha2512@users.noreply.github.com>
2021-02-02 20:39:25 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,75 @@
+import './App.css';
+import {useState,useEffect} from 'react';
+import io from "socket.io-client";
+const socket= io("http://localhost:9999/");
+function App() {
+  const [messages,setMessages]=useState([]);
+  const [chat,setChat]=useState("");
+  const [name,setName]=useState("");
+  const sendChat= ()=>{
+
+  }
+  const storeInput=(e)=>{
+    setChat(e.target.value);
+  }
+
+  const sendOnEnter = e =>{
+    if(e.keyCode===13)
+    sendChatToServer(chat)
+  }
+  const sendChatToServer =(msg)=>{
+    if(msg.trim()!==""){
+  socket.emit('mySocketEndPoint', {user:name,value:msg})
+      setChat("");
+    }
+  }
+
+ // const addMessage=msg=>{
+  //  setMessages([...messages,msg]);
+ // }
+
+useEffect(()=>{
+ fetch("http://localhost:9999/history").then(r=>r.json()).then((msg)=>{
+   setMessages(msg)
+ }).then(()=>{
+const inputName=prompt("enter your name");
+setName(inputName);
+socket.emit("Introduction",{user:inputName});
+
+socket.on('myBroadCast',(msg)=> {
+  setMessages(m=>[...m,msg])
+  console.log(msg);
+});
+ 
+
+/// intro broadcast 
+socket.on('broadcastIntro',(msg)=> {
+ //addMessage(msg);
+ setMessages(m=>[...m,msg]);
+ 
+});
+
+
+
+ })
+},[]);
+
+
+
+  return (
+    <div className="App">
+      <div id="name"></div>
+      <ul id="messages">{messages.map((msg,idx)=>{
+      <li>
+       <b>`${msg.name}</b>  ${msg.txt}` 
+      </li>
+      })}</ul>
+      <div  id="form" onKeyPress={sendOnEnter}>
+      <input id="input" onChange={storeInput} autoComplete="off" value={chat}/>
+      <button onClick={sendOnEnter}>Send</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
